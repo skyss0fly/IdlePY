a3f1d6a05f1ed34af95c5e8a532675a33e4e64cf
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-04-27 16:39:21 +1000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,8 +1,31 @@
 class LivingCreature():
   def __init__(self):
-    
+    self.name = 'Name'
   def __init__(health):
    self.health = 1
-  def __init__(name)
-    self.name = 'NAME'
+  def __init__(tire):
+    self.health = self.health - 1
+  def __init__(hurt):
+    self.health = self.health - 1
+
+class Player(LivingCreature):
+    def __init__(self, monster):
+    self.health = 15
+    self.name = input('what is your name?')
+    self.stamina = 100
+
+
+class Monster(LivingThing):
+    def __init__ (self,monster):
+        self.health = 15
+        self.name = name
+        self.Damage = Damage
+
+Commands = {
+ 'help', Player.help,
+ 'explore', Player.explore,
+ 'run', Player.explore,
+ 'fight', Player.fight,
+    }
+
    
