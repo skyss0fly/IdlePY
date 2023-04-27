b7a1959524a498299569f0d2bcf0e4fc948495f1
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-04-27 18:00:20 +1000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,5 +1,4 @@
-
-import Random
+import random
 class LivingCreature():
   def __init__(self):
       self.name = 'Name'
@@ -19,37 +18,36 @@ class Player(LivingCreature):
     def __init__(help):
         print('Here are your list of commands:')
         print('1. Help. 2.Explore. 3.run. 4.Fight')
-   def __init__(explore):
+    def __init__(explore):
         print(hero.name, 'Has gone Exploring! you will soon be faced with', monster.name)
         print('Stamina: ', stamina.amount, ' Health: ', health.amount)
-   def __init__(run):
+    def __init__(run):
         print('you Have ran from ', monster.name, 'and as a result, your stamina has decreased by 10')
         stamina.amount = stamina.amount - 10
         print('stamina: ', stamina.amount)
-  def __init__(fight):
+    def __init__(fight):
         print('You have fought', monster.name)
         self.health = random.randint(0,5)
         print('Your Health Currently: ', self.health)
   
-  if self.health == 0:
-        print('Game over!')
-class Monster(LivingThing):
+  
+class Monster(LivingCreature):
     def __init__ (self,monster):
         self.health = 15
         self.name = name
-        self.Damage = Damage
-Goblin=Monster('Goblin',20)
-Zombie=Monster('Zombie',15)
-
-Monster=[]
-
-Monsters= random.choice(Monster)
+        Goblin=Monster('Goblin',20)
+        Zombie=Monster('Zombie',15)
+        Monster=[]
+        Goblin.append
+        Zombie.append
+        Monsters= random.choice(Monster)
+        
 Commands = {
  'help', Player.help,
  'explore', Player.explore,
  'run', Player.run,
  'fight', Player.fight,
-    }
+ }
+ 
 
-
-   
+    
