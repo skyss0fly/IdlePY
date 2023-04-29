e6632531dedfee74c8412313b798ea564be86459
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-04-30 09:26:49 +1000

    Update Docs.py

--- Docs.py
+++ Docs.py
@@ -1,6 +1,6 @@
 class Documents():
   def __init__(self):
-      self.version = 0.1
+      self.version = 0.1.1
   def commands(self):
     print('You can use the following commands in game: help, run, fight, explore.')
       
