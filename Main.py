b51386801d279bfe5ac51c0e7146f31f96667a8e
skyss0fly <100278495+skyss0fly@users.noreply.github.com>
2023-05-09 13:20:44 +1000

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,1 +1,9 @@
 import pygame as pg
+
+class SoftwareRender:
+  def __init__(self):
+    self.RES = self.WIDTH, self.HEIGHT = 1600,900
+    self.H_WIDTH, self.H_HEIGHT = self.WIDTH // 2, self.HEIGHT //2
+    self.FPS = 60
+    self.screen = pg.display.set_mode(self.RES)
+    self.clock = pg.time.Clock()
